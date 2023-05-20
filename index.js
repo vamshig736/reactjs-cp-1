14af062bcb24414057d040c128079159b80d5496
vamshi krishna <96162476+vamshig736@users.noreply.github.com>
2023-05-20 20:29:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const element = (
   //   Write your code here.
   <div className="bg-container">
     <h1 className="heading">Super Over League</h1>
-    <div className="image-container d-flex flex-row justify-content-center">
+    <div className="image-container">
       <img
         src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
-        className="image"
+        className="logo-image"
       />
       <img
         src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
-        className="image"
+        className="logo-image"
       />
     </div>
   </div>
